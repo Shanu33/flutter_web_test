19d0a2270332304da38ff72fbee850ed7400695b
Shanu <Shahnawaz.sk230@gmail.com>
2025-07-02 00:58:15 +0530

    Login label  changed and space between Sing in option button's added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2725602286"
+    serviceWorkerVersion: "966205214"
   }
 });
