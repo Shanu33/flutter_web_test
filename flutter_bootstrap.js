f72cade7b0a89264a519907b54b094fd0132e6d4
Shanu <Shahnawaz.sk230@gmail.com>
2025-07-03 16:46:32 +0530

    login line reponsiveness improved

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3133239532"
+    serviceWorkerVersion: "1201098526"
   }
 });
